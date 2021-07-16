db85e16b326bb313a25cba3ec62df63f2288a60f
ITzTravelIntime <23339020+ITzTravelInTime@users.noreply.github.com>
2021-07-16 10:23:02 +0200

    Version 2.2.2
    - Updated platform requirements

--- Package.swift
+++ Package.swift
@@ -5,6 +5,10 @@ import PackageDescription
 
 let package = Package(
     name: "SwiftLoggedPrint",
+    platforms: [
+        .macOS("10.9"),
+        .iOS("7.0")
+    ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
